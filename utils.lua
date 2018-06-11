92a90d1ccf9ba1bc63c1456c2d744d8d177b3bad
Paul Hagedorn <panzer1119@gmail.com>
2018-06-11 19:42:06 +0200

    Added support for a path in the program name

--- utils.lua
+++ utils.lua
@@ -2,7 +2,7 @@
 
   Author: Panzer1119
   
-  Date: Edited 11 Jun 2018 - 07:38 PM
+  Date: Edited 11 Jun 2018 - 07:41 PM
   
   Original Source: https://github.com/Panzer1119/CCUtils/blob/master/utils.lua
   
@@ -22,5 +22,14 @@ function getRunningProgram()
 end
 
 function getRunningProgramWithExtension()
+  local path = getRunningProgramWithPathAndExtension()
+  local index = su.findLast(path, "%/")
+  if (index ~= nil and index > 0) then
+	path = string.sub(path, index + 1)
+  end
+  return path
+end
+
+function getRunningProgramWithPathAndExtension()
   return shell.getRunningProgram()
 end
