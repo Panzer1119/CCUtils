670452f32494532e2e9e87f2b21e86e2f74b2205
Paul Hagedorn <panzer1119@gmail.com>
2018-06-11 19:23:09 +0200

    Added information
    Author etc.

--- su.lua
+++ su.lua
@@ -1,3 +1,15 @@
+--[[
+
+  Author: Panzer1119
+  
+  Date: Edited 11 Jun 2018 - 07:22 PM
+  
+  Original Source: https://github.com/Panzer1119/CCUtils/blob/master/su.lua
+  
+  Direct Download: https://raw.githubusercontent.com/Panzer1119/CCUtils/blob/master/su.lua
+
+]]--
+
 function isempty(s)
   return s == nil or s == ''
 end
