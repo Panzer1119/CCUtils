eecaaa2cd5dfef9bfdff62d98be2cb4ea6b62452
Paul Hagedorn <panzer1119@gmail.com>
2018-06-11 19:28:03 +0200

    Added findLast method

--- su.lua
+++ su.lua
@@ -2,7 +2,7 @@
 
   Author: Panzer1119
   
-  Date: Edited 11 Jun 2018 - 07:22 PM
+  Date: Edited 11 Jun 2018 - 07:27 PM
   
   Original Source: https://github.com/Panzer1119/CCUtils/blob/master/su.lua
   
@@ -12,4 +12,9 @@
 
 function isEmpty(s)
   return s == nil or s == ''
+end
+
+function findLast(haystack, needle)
+    local i = haystack:match(".*"..needle.."()")
+    if i == nil then return nil else return i-1 end
 end
