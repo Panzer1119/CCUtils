16e29620fa99766f24c63afbd5750eee056408aa
Paul Hagedorn <panzer1119@gmail.com>
2018-06-11 19:25:24 +0200

    Added information
    Author etc.

--- utils.lua
+++ utils.lua
@@ -1,8 +1,22 @@
+--[[
+
+  Author: Panzer1119
+  
+  Date: Edited 11 Jun 2018 - 07:24 PM
+  
+  Original Source: https://github.com/Panzer1119/CCUtils/blob/master/utils.lua
+  
+  Direct Download: https://raw.githubusercontent.com/Panzer1119/CCUtils/blob/master/utils.lua
+
+]]--
+
+os.loadAPI("su.lua")
+
 function getRunningProgram()
   local name = getRunningProgramWithExtension()
   local lastIndex = 0
   local index = string.find(name, ".")
-  if (index != nil) then
+  if (index[0] != nil) then
     while (index != (index = string.find(name, ".", lastIndex))) do
       lastIndex = index[1]
     end
