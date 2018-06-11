c10b8b5a3a541a1c8a51263975e806660eaacc72
Paul Hagedorn <panzer1119@gmail.com>
2018-06-11 19:38:37 +0200

    Fixed the getRunningProgram methods

--- utils.lua
+++ utils.lua
@@ -2,7 +2,7 @@
 
   Author: Panzer1119
   
-  Date: Edited 11 Jun 2018 - 07:28 PM
+  Date: Edited 11 Jun 2018 - 07:38 PM
   
   Original Source: https://github.com/Panzer1119/CCUtils/blob/master/utils.lua
   
@@ -14,14 +14,13 @@ os.loadAPI("su.lua")
 
 function getRunningProgram()
   local name = getRunningProgramWithExtension()
-  local lastIndex = 0
-  local index = su.findLast(name, ".")
-  print(index)
+  local index = su.findLast(name, "%.")
+  if (index ~= nil and index > 0) then
+	name = string.sub(name, 1, index - 1)
+  end
   return name
 end
 
 function getRunningProgramWithExtension()
   return shell.getRunningProgram()
 end
-
-getRunningProgram()
