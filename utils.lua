3520a7d9df664967e4bee22172e1cd2d479788ec
Paul Hagedorn <panzer1119@gmail.com>
2018-06-11 19:28:49 +0200

    Some fixes

--- utils.lua
+++ utils.lua
@@ -2,7 +2,7 @@
 
   Author: Panzer1119
   
-  Date: Edited 11 Jun 2018 - 07:24 PM
+  Date: Edited 11 Jun 2018 - 07:28 PM
   
   Original Source: https://github.com/Panzer1119/CCUtils/blob/master/utils.lua
   
@@ -15,16 +15,13 @@ os.loadAPI("su.lua")
 function getRunningProgram()
   local name = getRunningProgramWithExtension()
   local lastIndex = 0
-  local index = string.find(name, ".")
-  if (index[0] != nil) then
-    while (index != (index = string.find(name, ".", lastIndex))) do
-      lastIndex = index[1]
-    end
-    name = string.sub(name, lastIndex)
-  end
+  local index = su.findLast(name, ".")
+  print(index)
   return name
 end
 
 function getRunningProgramWithExtension()
   return shell.getRunningProgram()
 end
+
+getRunningProgram()
