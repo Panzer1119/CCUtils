132f43e080b3bf7b7b34edee691fa8e2c134bb99
Paul Hagedorn <panzer1119@gmail.com>
2018-06-23 18:36:26 +0200

    Edited sounds.lua
    Added playSound functions

--- sounds.lua
+++ sounds.lua
@@ -1,11 +1,25 @@
 --[[
   Author: Panzer1119
   
-  Date: Created 23 Jun 2018 - 06:30 PM
+  Date: Edited 23 Jun 2018 - 06:36 PM
   
   Original Source: https://github.com/Panzer1119/CCUtils/blob/master/sounds.lua
   
-  Direct Download: https://raw.githubusercontent.com/Panzer1119/CCUtils/blob/master/sounds.lua
+  Direct Download: https://raw.githubusercontent.com/Panzer1119/CCUtils/master/sounds.lua
 ]]--
 
 sounds = {}
+
+function loadSounds(filename)
+  local f = fs.open(filename, "r")
+  sounds = textutils.unserialise(f.readAll())
+  f.close()
+end
+
+function playSound(soundname, source, volume)
+  shell.run("exec playSound " .. soundname .. " " .. source .. " @a ~ ~ ~ " .. volume)
+end
+
+function playContinousSound(soundname, source, volume)
+  playSound(soundname, source, volume)
+end
