992f14f5b4fd30f5cc8f56ecfd1ce23acc8aa3f2
Paul Hagedorn <panzer1119@gmail.com>
2018-06-23 19:03:42 +0200

    Edited sounds.lua
    Added more playSound functions

--- sounds.lua
+++ sounds.lua
@@ -1,7 +1,7 @@
 --[[
   Author: Panzer1119
   
-  Date: Edited 23 Jun 2018 - 06:36 PM
+  Date: Edited 23 Jun 2018 - 07:01 PM
   
   Original Source: https://github.com/Panzer1119/CCUtils/blob/master/sounds.lua
   
@@ -16,10 +16,60 @@ function loadSounds(filename)
   f.close()
 end
 
-function playSound(soundname, source, volume)
-  shell.run("exec playSound " .. soundname .. " " .. source .. " @a ~ ~ ~ " .. volume)
+function playSound(shell, soundname, source, target, place, volume, pitch, minimumVolume)
+  shell.run("exec playSound " .. soundname .. " " .. source .. " " .. target .. " " .. place .. " " .. volume .. " " .. pitch .. " " .. minimumVolume)
 end
 
-function playContinousSound(soundname, source, volume)
-  playSound(soundname, source, volume)
+function playSound(shell, soundname, source, target, place, volume, pitch)
+  shell.run("exec playSound " .. soundname .. " " .. source .. " " .. target .. " " .. place .. " " .. volume .. " " .. pitch)
+end
+
+function playSound(shell, soundname, source, target, place, volume)
+  shell.run("exec playSound " .. soundname .. " " .. source .. " " .. target .. " " .. place .. " " .. volume)
+end
+
+function playSound(shell, soundname, source, target, place)
+  shell.run("exec playSound " .. soundname .. " " .. source .. " " .. target .. " " .. place)
+end
+
+function playSoundHere(shell, soundname, source, target)
+  shell.run("exec playSound " .. soundname .. " " .. source .. " " .. target)
+end
+
+function playContinousSound(shell, soundname, source, target, place, volume)
+  local sound = getSoundByName(soundname)
+  local playTime = sound.length
+  local pauseTime = sound.pause
+  local sleepTime = playTime + pauseTime
+  while true do
+    playSound(shell, soundname, source, target, place, volume)
+	sleep(sleepTime)
+  end
+end
+
+function playSoundAndWaitForLoop(shell, soundname, source, target, place, volume)
+  local sound = getSoundByName(soundname)
+  local playTime = sound.length
+  local pauseTime = sound.pause
+  local sleepTime = playTime + pauseTime
+  playSound(shell, soundname, source, target, place, volume)
+  sleep(sleepTime)
+end
+
+function playSoundAndWait(shell, soundname, source, target, place, volume)
+  local sound = getSoundByName(soundname)
+  local playTime = sound.length
+  playSound(shell, soundname, source, target, place, volume)
+  sleep(playTime)
+end
+
+function getSoundByName(soundname)
+  if (#sounds == 0) then
+    return nil
+  end
+  for i = 1, #sounds do
+    if (sounds[i].name == soundname) then
+	  return sounds[i]
+	end
+  end
 end
