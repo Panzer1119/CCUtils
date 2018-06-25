b74c16121c9265ec6b89873c92f472b639087efd
Unknown <panzer1119@gmail.com>
2018-06-25 19:54:40 +0200

    Fixed mission shell statement

--- install.lua
+++ install.lua
@@ -1,7 +1,7 @@
 --[[
   Author: Panzer1119
   
-  Date: Edited 25 Jun 2018 - 07:53 PM
+  Date: Edited 25 Jun 2018 - 07:54 PM
   
   Original Source: https://github.com/Panzer1119/CCUtils/blob/master/install.lua
   
@@ -9,7 +9,7 @@
 ]]--
 
 filename_files = "downloads/files.lon"
-wget https://raw.githubusercontent.com/Panzer1119/CCUtils/master/files.lon filename_files
+shell.run("wget https://raw.githubusercontent.com/Panzer1119/CCUtils/master/files.lon filename_files")
 
 local f = fs.open(filename_files, "r")
 files = textutils.unserialise(f.readAll())
