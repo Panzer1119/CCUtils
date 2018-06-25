dd8d7d540c3954a92b799793db9f196cf16f1f22
Unknown <panzer1119@gmail.com>
2018-06-25 19:47:42 +0200

    Created files.lon

--- install.lua
+++ install.lua
@@ -1,9 +1,14 @@
 --[[
   Author: Panzer1119
   
-  Date: Created 25 Jun 2018 - 07:45 PM
+  Date: Edited 25 Jun 2018 - 07:46 PM
   
   Original Source: https://github.com/Panzer1119/CCUtils/blob/master/install.lua
   
   Direct Download: https://raw.githubusercontent.com/Panzer1119/CCUtils/master/install.lua
 ]]--
+
+filename_files = "downloads/files.lon"
+wget  filename_files
+
+local f = fs.open(filename_files, "r")
