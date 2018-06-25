8772a265d550a74ccef3eedbcbedf838cbafc4f4
Unknown <panzer1119@gmail.com>
2018-06-25 20:16:27 +0200

    Improved install.lua

--- install.lua
+++ install.lua
@@ -1,7 +1,7 @@
 --[[
   Author: Panzer1119
   
-  Date: Edited 25 Jun 2018 - 08:02 PM
+  Date: Edited 25 Jun 2018 - 08:15 PM
   
   Original Source: https://github.com/Panzer1119/CCUtils/blob/master/install.lua
   
@@ -16,8 +16,49 @@ local f = fs.open(filename_files, "r")
 files = textutils.unserialise(f.readAll())
 f.close()
 
+installed = true
+
 for i = 1, #files do
+	print("")
+	local download = true
 	local file = files[i]
-	print("Downloading " .. file.name .. " to " .. file.path .. "...")
-	shell.run("wget " .. file.url .. " " .. file.path)
+	if (fs.exists(file.path)) then
+		download = false
+		print(file.name .. " is already installed,")
+		write("do you want to update it? (Y/n): ")
+		local input = read()
+		if (input == "Y") then
+			fs.delete(file.path)
+			download = true
+		end
+	end
+	if (download) then
+		print("Downloading " .. file.name .. " to " .. file.path .. "...")
+		shell.run("wget " .. file.url .. " " .. file.path)
+		if not (fs.exists(file.path)) then
+			installed = false
+		end
+	end
+end
+
+if (installed) then
+	print("")
+	print("")
+	print("Installation was successful, do you want to")
+	print("delete the cache and installation files?")
+	write("(the installation file can be used to update the library) (Y/n): ")
+	local input = read()
+	if (input == "Y") then
+		shell.run("delete " .. filename_files)
+		shell.run("delete " .. shell.getRunningProgram())
+	end
+else
+	print("")
+	print("")
+	print("Some files failed and could not be installed,")
+	write("do you want to try it again? (Y/n): ")
+	local input = read()
+	if (input == "Y") then
+		shell.run(shell.getRunningProgram())
+	end
 end
