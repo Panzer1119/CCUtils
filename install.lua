631e4ffc49e5c2a891486a277b7d3f15ff6ee9c3
Unknown <panzer1119@gmail.com>
2018-06-25 19:53:55 +0200

    Edited install.lua

--- install.lua
+++ install.lua
@@ -1,7 +1,7 @@
 --[[
   Author: Panzer1119
   
-  Date: Edited 25 Jun 2018 - 07:46 PM
+  Date: Edited 25 Jun 2018 - 07:53 PM
   
   Original Source: https://github.com/Panzer1119/CCUtils/blob/master/install.lua
   
@@ -9,6 +9,12 @@
 ]]--
 
 filename_files = "downloads/files.lon"
-wget  filename_files
+wget https://raw.githubusercontent.com/Panzer1119/CCUtils/master/files.lon filename_files
 
 local f = fs.open(filename_files, "r")
+files = textutils.unserialise(f.readAll())
+f.close()
+
+for i = 1, #files do
+	print(textutils.serialise(files[i]))
+end
