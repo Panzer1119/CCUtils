d42be380577d17893a2f45f053c7c8f426a60508
Unknown <panzer1119@gmail.com>
2018-06-29 01:41:42 +0200

    Added multiple charsets

--- lib/su.lua
+++ lib/su.lua
@@ -2,7 +2,7 @@
 
   Author: Panzer1119
   
-  Date: Edited 26 Jun 2018 - 02:49 AM
+  Date: Edited 29 Jun 2018 - 01:41 AM
   
   Original Source: https://github.com/Panzer1119/CCUtils/blob/master/lib/su.lua
   
@@ -10,10 +10,43 @@
 
 ]]--
 
-charset_reverse = {}  do 
-	for c = 48, 57  do charset_reverse[string.char(c)]=c end
-	for c = 65, 90  do charset_reverse[string.char(c)]=c end
-	for c = 97, 122 do charset_reverse[string.char(c)]=c end
+charset_numerical_alpha_big_alpha = {}  do -- [0-9a-zA-Z]
+    for c = 48, 57  do table.insert(charset_numerical_alpha_big_alpha, string.char(c)) end
+    for c = 65, 90  do table.insert(charset_numerical_alpha_big_alpha, string.char(c)) end
+    for c = 97, 122 do table.insert(charset_numerical_alpha_big_alpha, string.char(c)) end
+end
+
+charset_alpha_big_alpha = {}  do -- [a-zA-Z]
+    for c = 65, 90  do table.insert(charset_alpha_big_alpha, string.char(c)) end
+    for c = 97, 122 do table.insert(charset_alpha_big_alpha, string.char(c)) end
+end
+
+charset_numerical_alpha = {}  do -- [0-9a-z]
+    for c = 48, 57  do table.insert(charset_numerical_alpha, string.char(c)) end
+    for c = 65, 90  do table.insert(charset_numerical_alpha, string.char(c)) end
+end
+
+charset_numerical_big_alpha = {}  do -- [0-9A-Z]
+    for c = 48, 57  do table.insert(charset_numerical_big_alpha, string.char(c)) end
+    for c = 97, 122 do table.insert(charset_numerical_big_alpha, string.char(c)) end
+end
+
+charset_numerical = {}  do -- [0-9]
+    for c = 48, 57  do table.insert(charset_numerical, string.char(c)) end
+end
+
+charset_alpha = {}  do -- [a-z]
+    for c = 65, 90  do table.insert(charset_alpha, string.char(c)) end
+end
+
+charset_big_alpha = {}  do -- [A-Z]
+    for c = 97, 122 do table.insert(charset_big_alpha, string.char(c)) end
+end
+
+charset_numerical_alpha_big_alpha_inverse = {}  do 
+	for c = 48, 57  do charset_numerical_alpha_big_alpha_inverse[string.char(c)]=c end
+	for c = 65, 90  do charset_numerical_alpha_big_alpha_inverse[string.char(c)]=c end
+	for c = 97, 122 do charset_numerical_alpha_big_alpha_inverse[string.char(c)]=c end
 end
 
 function isEmpty(s)
@@ -25,13 +58,7 @@ function findLast(haystack, needle)
     if i == nil then return nil else return i-1 end
 end
 
-charset = {}  do -- [0-9a-zA-Z]
-    for c = 48, 57  do table.insert(charset, string.char(c)) end
-    for c = 65, 90  do table.insert(charset, string.char(c)) end
-    for c = 97, 122 do table.insert(charset, string.char(c)) end
-end
-
-function randomString(length)
+function randomString(charset, length)
     if not length or length <= 0 then return '' end
 	if length >= 245 then return nil end
     math.randomseed(os.clock()^5)
@@ -44,7 +71,7 @@ function sumString(text)
 	end
 	local sum = 0
 	for c in text:gmatch"." do
-		sum = sum + charset_reverse[c]
+		sum = sum + charset_big_alpha_numerical_inverse[c]
 	end
 	return sum
 end
