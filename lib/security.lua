b1ca251b0d4608ae36f9a41593608d6a878aa9df
Unknown <panzer1119@gmail.com>
2018-06-29 20:30:52 +0200

    Created security.lua

---
+++ lib/security.lua
@@ -0,0 +1,13 @@
+--[[
+
+  Author: Panzer1119
+  
+  Date: Created 29 Jun 2018 - 10:30 PM
+  
+  Original Source: https://github.com/Panzer1119/CCUtils/blob/master/lib/security.lua
+  
+  Direct Download: https://raw.githubusercontent.com/Panzer1119/CCUtils/master/lib/security.lua
+
+]]--
+
+os.loadAPI("lib/utils.lua")
