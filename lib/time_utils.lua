991dab820f90d36871599cae9962ea47f5e814a0
Paul Hagedorn <panzer1119@gmail.com>
2019-07-10 21:47:34 +0200

    Created lib/time_utils.lua

---
+++ lib/time_utils.lua
@@ -0,0 +1,11 @@
+--[[
+
+  Author: Panzer1119
+  
+  Date: Created 10 Jul 2019 - 09:46 PM
+  
+  Original Source: https://github.com/Panzer1119/CCUtils/blob/master/lib/time_utils.lua
+  
+  Direct Download: https://raw.githubusercontent.com/Panzer1119/CCUtils/master/lib/time_utils.lua
+
+]]--
--- lib/utils.lua
+++ lib/utils.lua
@@ -2,7 +2,7 @@
 
   Author: Panzer1119
   
-  Date: Edited 29 Jun 2018 - 02:32 AM
+  Date: Edited 10 Jul 2019 - 09:45 PM
   
   Original Source: https://github.com/Panzer1119/CCUtils/blob/master/lib/utils.lua
   
@@ -11,6 +11,7 @@
 ]]--
 
 os.loadAPI("lib/su.lua")
+os.loadAPI("lib/time_utils.lua")
 
 sides = {"top", "bottom", "left", "right", "back", "front"}
 
